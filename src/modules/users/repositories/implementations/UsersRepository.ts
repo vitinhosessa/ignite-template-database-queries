ffa8fc4522937b7b79deefe0d7b304d7979d7715
Vitinho <vitinho.dev@gmail.com>
2021-03-29 17:15:19 -0300

    Consegui :)

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const id = user_id
+    const user = await this.repository.createQueryBuilder("users")
+    .leftJoinAndSelect("users.games", "games")
+    .where("users.id = :id", { id: `${user_id}`})
+    .getOne()
+
+    if(!user) throw new Error("User don't exists");
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query("SELECT * FROM users ORDER BY first_name ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`SELECT * FROM users WHERE LOWER(first_name) = LOWER('${first_name}') OR LOWER(last_name) = LOWER('${last_name}')`); // Complete usando raw query
   }
 }
